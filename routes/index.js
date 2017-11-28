41796458f18d8d584022893f13a43b5afb097030
Roya Rahmati <Roya@Royas-MacBook-Pro.local>
2017-11-28 13:24:20 +0100

    resolving master change conflict

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,11 @@
 var express = require('express');
-var path = require('path');
-//var app = express();
+var connection = require('../config/db.js');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    //res.render('index', { title: 'Express' });
-    // console.log('I am here!');
-    // console.log(path.join(__dirname, '../views'));
-    res.sendFile(path.join(__dirname, '../views/page.html'));
-    //res.sendFile( 'page.html');
+  res.render('index', { title: 'Express' });
 });
-//app.use('/', router);
+
 module.exports = router;
