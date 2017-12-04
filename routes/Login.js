02e2566ac51c83d338cca8013474ed740ad00174
anjalisasi <anjali.sasidharan@ymail.com>
2017-12-05 00:04:46 +0100

    login

--- routes/Login.js
+++ routes/Login.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 /*
-
  */
 var mongoose = require('mongoose');
 var url = 'mongodb://admin:admin@ds249005.mlab.com:49005/pm102realtimeauction';
@@ -20,28 +19,34 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
     UserName : String,
-    Password: String
+    Password: String,
+    userRole: String,
+    firstName:String,
+    LastName:String
 });
 
 // Compile model from schema
-var UserModel = mongoose.model('UserModel', UserSchema);
+var users = mongoose.model('users', UserSchema);
 
 
 // Create an instance of model SomeModel
-var user1 = new UserModel({
-    UserName : "kiel",
-    Password: "123"
+var user1 = new users({
+    UserName : "admin@auction.com",
+    Password: "123",
+    userRole:"Admin",
+    firstName:"",
+    LastName:""
 });
-// user1.save(function (err) {
-//     if (err) {
-//       console.log("Error while inserting user: ", err);
-//     }
-//     else
-//     {
-//         console.log("User saved")
-//     }
-//     // saved!
-// });
+ user1.save(function (err) {
+     if (err) {
+       console.log("Error while inserting user: ", err);
+     }
+     else
+     {
+         console.log("User saved")
+     }
+     // saved!
+ });
 
 
 
@@ -51,11 +56,11 @@ var user1 = new UserModel({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login');
+    res.render('login');
 });
 
 router.post('/Login', function(req, res, next) {
-  //TODO:
+    //TODO:
     //1: Get data from form objects
     var userName = req.body.userName;
     var password = req.body.password;
@@ -63,39 +68,29 @@ router.post('/Login', function(req, res, next) {
     console.log('User Name: ',userName);
     console.log('password: ', password);
     //2: Check if the user exist
-    UserModel.findOne({UserName: userName, Password:password}).exec(function(err, user) {
+    users.findOne({UserName: userName, Password:password}).exec(function(err, user) {
         if (err) {
             console.log('Error while getting a user from DB');
         } else {
             console.log('no error');
             //console.log('user ', user);
             if(user){
-               // console.log('user ', user);
+                // console.log('user ', user);
                 res.render('index', { title: 'Welcome '+ user.UserName });
             }
             else {
-                console.log('user not exist ');
+                console.log('user does not exist Please Sign Up');
+                res.render('signup');
             }
 
-            //3: Pass product to view (product.pug)
-
 
         }
     });
 
-    usermodel.find({}).exec(function(err, user) {
-        if (err) {
-            console.log('Error while to SignUp');
-        } else {
-            console.log('Sign Up for free');
-            res.render('signup', {title: 'SignUp page', data: user});
-        }
+    router.post('/SignUp', function(req, res, next) {
+        res.render('index', { title: 'please Sign Up ' });
     });
 
 
-            //3: if user exist login him to index view
-
-    //4: if user does not exist prevent him to login and show him a message
-    // res.render('login');
 });
 module.exports = router;
